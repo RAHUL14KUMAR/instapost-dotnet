7009316534403566786cf03bf95c102e15e39fae
RAHUL14KUMAR <rahul.kumar.it25@heritageit.edu.in>
2025-08-16 16:25:26 +0530

    make connection with pg

--- instapost/Program.cs
+++ instapost/Program.cs
@@ -1,9 +1,21 @@
+using instapostBusinesslayer.Interface.CategoryInterface;
+using instapostBusinesslayer.Repository;
+using instapostBusinesslayer.Service.Implementation;
+using instapostBusinesslayer.Service.Interface;
+using instapostDataLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(Options => Options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<CategoryIRepository, CategoryRepository>();
+builder.Services.AddScoped<CategorySInterface, CategoryService>();
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
